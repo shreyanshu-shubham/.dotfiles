c212b9ef84cd1a58d450c5c788d5ad2cd488557c
Shreyanshu Shubham <sshubham@zscaler.com>
2025-07-31 17:20:47 +0530

    update

---
+++ bash/.bash_aliases
@@ -0,0 +1,18 @@
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# adding flags
+alias df='df -h'               # human-readable sizes
+alias free='free -m'           # show sizes in MB
+alias grep='grep --color=auto' # colorize output (good for log files)
+alias mv='mv -i'               # interactive don't override
+alias cp='cp -L'
+
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,70 @@
 #!/bin/bash
 
+###################################################################################################################
+# UTIL FUNCTIONS
+###################################################################################################################
+
+function get_latest_tag_for_remote(){
+    local remote_url=$1
+    latest_tag=$(git ls-remote --tags --sort='v:refname' "${remote_url}" | grep -vE "\^\{\}$"  | tail -n1 |awk -F' ' '{print $NF}' | cut -d '/' -f3)
+    printf "%s" "${latest_tag}"
+}
+
+function print_separator(){
+    local separator_width=$1
+    local separator_character=$2
+    if [[ ${separator_width} -le 1 ]]; then
+        return 1
+    fi
+    if [[ ${#separator_character} -ne 1 ]]; then
+        return 1
+    fi
+    printf -v separator_string "%*s" "${separator_width}" ""
+    echo "${separator_string}" | tr " " "${separator_character}"
+}
+
+function print_separator_with_string(){
+    local separator_width=$1
+    local separator_character=$2
+    local string=$3
+    if [[ ${separator_width} -le $(( ${#string} + 3 )) ]]; then
+        return 1
+    fi
+    if [[ ${#separator_character} -ne 1 ]]; then
+        return 1
+    fi
+    printf -v separator_string "%*s" $(( separator_width - ${#string} - 3 )) ""
+    separator_string=$(echo -n "${separator_string}" | tr " " "${separator_character}" )
+    echo "${separator_character} ${string} ${separator_string}"
+}
+
+function print_left_padded_string(){
+    local pad_length=$1
+    local pad_character=$2
+    local string=$3
+    printf -v pad_string "%*s" $(( pad_length - ${#string} )) ""
+    pad_string=$(echo -n "${pad_string}" | tr " " "${pad_character}" )
+    echo "${string}${pad_string}"
+}
+
+function print_right_padded_string(){
+    local pad_length=$1
+    local pad_character=$2
+    local string=$3
+    printf -v pad_string "%*s" $(( pad_length - ${#string} )) ""
+    pad_string=$(echo -n "${pad_string}" | tr " " "${pad_character}" )
+    echo "${pad_string}${string}"
+}
+
+###################################################################################################################
+# INSTALL FUNCTIONS
+###################################################################################################################
+
 function install_node() {
+    latest_tag=$(get_latest_tag_for_remote https://github.com/nvm-sh/nvm.git)
+
     # installs nvm (Node Version Manager)
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${latest_tag}/install.sh" | bash
     # download and install Node.js (you may need to restart the terminal)
     nvm install 20
     # verifies the right Node.js version is in the environment
@@ -12,9 +74,11 @@ function install_node() {
 }
 
 function install_neovim() {
-    wget --output-document="${CUSTOM_CONFIG_ROOT}/nvim-linux64.tar.gz" https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
-    pwd
-    tar xzvf "${CUSTOM_CONFIG_ROOT}/nvim-linux64.tar.gz"
+    latest_tag=$(get_latest_tag_for_remote https://github.com/neovim/neovim.git)
+    git clone --branch "${latest_tag}" --single-branch https://github.com/neovim/neovim.git 
+    cd neovim
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
 }
 
 function install_custom_postgres_version(){
@@ -31,28 +95,41 @@ function install_custom_postgres_version(){
     sudo apt update
 }
 
-mkdir -p "$CUSTOM_CONFIG_ROOT"
-cd "$CUSTOM_CONFIG_ROOT"
+function install_fzf(){
+    rm -rf ~/.fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
 
-# sudo apt install -y neofetch tree fzf git gcc g++ python3-full curl wget unzip file net-tools
-# install_node
+function install_fastfetch(){
+    # TODO : implementation pending 
+    :
+}
 
-install_neovim
+TERMINAL_COLUMNS=$(tput cols)
+TERMINAL_ROWS=$(tput lines)
+CUSTOM_CONFIG_SOURCE_ROOT=$(dirname "$(realpath "$0")")
 
-# mkdir -p ~/.config/nvim
-# ln nvim-config/init.lua ~/.config/nvim/init.lua
+# mkdir -p "$CUSTOM_CONFIG_ROOT"
+# cd "$CUSTOM_CONFIG_ROOT"
 
-# echo "$BASHRC_APPEND" >> ~/.bashrc_test
+# install_neovim
 
-# install from apt
-packages_to_install=("tldr" "wget" "git" "curl" "gcc" "g++" "make" "cmake")
+# # mkdir -p ~/.config/nvim
+# # ln nvim-config/init.lua ~/.config/nvim/init.lua
 
-sudo apt -qq update
-for itm in "${packages_to_install[@]}"; do
-	echo "Installing ${itm}"
-	sudo apt install -qq -y ${itm}
-done
+# # echo "$BASHRC_APPEND" >> ~/.bashrc_test
 
-tldr -u
+# # install from apt
+# packages_to_install=("tldr" "wget" "git" "curl" "gcc" "g++" "make" "cmake" "fzf" "curl" "unzip" "net-tools")
 
-# TODO git config 
+# sudo apt -qq update
+# for itm in "${packages_to_install[@]}"; do
+# 	echo "Installing ${itm}"
+# 	sudo apt install -qq -y ${itm}
+# done
+
+# tldr -u
+
+
+print_separator_with_string "2" '0' "hello"
